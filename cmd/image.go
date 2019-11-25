3dea44d76c8f14d8ca9facebe645bd1b13db8fa0
John Claro <jkrclaro@gmail.com>
2019-11-25 17:52:05 +0000

    Iterate through each favicon format and save them to test folder

--- cmd/image.go
+++ cmd/image.go
@@ -27,8 +27,8 @@ import (
 var imageCmd = &cobra.Command{
 	Use:   "image",
 	Short: "Convert an image to its favicon formats.",
-	Long: `Convert an image that you want to use for your
-	favicon then use this tool to convert an image to its favicon formats.`,
+	Long: `Convert an image that you want to use as your favicon then use this 
+	tool to convert an image to its favicon formats.`,
 	Args: func(cmd *cobra.Command, args []string) error {
 		fmt.Println(len(args))
 		if len(args) < 2 {
@@ -46,11 +46,33 @@ var imageCmd = &cobra.Command{
 			log.Fatalf("Failed to open image: %v", err)
 		}
 
-		src = imaging.Resize(src, 16, 16, imaging.Lanczos)
+		files := map[string]int{
+			"apple-touch-icon-57x57.png":   57,
+			"apple-touch-icon-60x60.png":   60,
+			"apple-touch-icon-72x72.png":   72,
+			"apple-touch-icon-76x76.png":   76,
+			"apple-touch-icon-114x114.png": 114,
+			"apple-touch-icon-120x120.png": 120,
+			"apple-touch-icon-144x144.png": 144,
+			"apple-touch-icon-152x152.png": 152,
+			"favicon-16x16.png":            16,
+			"favicon-32x32.png":            32,
+			"favicon-96x96.png":            96,
+			"favicon-128.png":              128,
+			"favicon-196x196.png":          196,
+			"mstile-70x70.png":             70,
+			"mstile-150x150.png":           150,
+			"mstile-310x310.png":           310,
+			// TODO: "mstile-310x150.png":
+		}
 
-		err = imaging.Save(src, "output.png")
-		if err != nil {
-			log.Fatalf("Failed to save image: %v", err)
+		for filename, dimension := range files {
+			filepath := "./test/" + filename
+			src = imaging.Resize(src, dimension, dimension, imaging.Lanczos)
+			err = imaging.Save(src, filepath)
+			if err != nil {
+				log.Fatalf("Failed to save image: %v", err)
+			}
 		}
 	},
 }
